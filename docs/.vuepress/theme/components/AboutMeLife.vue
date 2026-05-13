b6c2d843e3ea5c16e01e1dc8f97cb01f3c28a900
Withnoidea <2482454653@qq.com>
2026-05-13 15:03:26 +0800

    Update AboutMeLife.vue

--- docs/.vuepress/theme/components/AboutMeLife.vue
+++ docs/.vuepress/theme/components/AboutMeLife.vue
@@ -9,53 +9,51 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
-import * as echarts from 'echarts';
+import { ref, onMounted } from "vue";
+import * as echarts from "echarts";
 
 const chartRef = ref(null);
 
 // 示例数据
 const lifeData = [
-  {name: '影视', value: 1},
-  {name: '音乐', value: 1},
-  {name: '游戏', value: 2},
-  {name: '阅读', value: 2},
-  {name: '摄影', value: 2},
-  {name: '健身', value: 2},
-  {name: '工作', value: 2},
-  {name: '学习', value: 6}
+  { name: "影视", value: 1 },
+  { name: "游戏", value: 1 },
+  { name: "健身", value: 2 },
+  { name: "音乐", value: 3 },
+  { name: "摄影", value: 3 },
+  { name: "工作", value: 4 },
+  { name: "学习", value: 6 },
 ];
 onMounted(() => {
   const myChart = echarts.init(chartRef.value);
 
   const option = {
     tooltip: {
-      trigger: 'item'
+      trigger: "item",
     },
     series: [
       {
-        name: '我的日常',
-        type: 'pie',
-        radius: ['50%', '80%'],
-        center: ['50%', '60%'],
+        name: "我的日常",
+        type: "pie",
+        radius: ["50%", "80%"],
+        center: ["50%", "60%"],
         startAngle: 180,
         endAngle: 360,
-        data: lifeData
-      }
-    ]
+        data: lifeData,
+      },
+    ],
   };
 
   myChart.setOption(option);
 
   // 响应式调整
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     myChart.resize();
   });
 });
 </script>
 
 <style scoped>
-
 .about-me-life {
   height: 316px;
   padding: 20px 0;
